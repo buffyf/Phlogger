47108b9f216d68722ebd4923b6eeb33567da54eb
Buffy Feinstein <buffyfeinstein@gmail.com>
2019-04-20 16:52:52 -0500

    Initial Commit

---
+++ Phlogger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Phlogger
+//
+//  Created by Barbara Feinstein on 4/20/19.
+//  Copyright © 2019 Barbara Feinstein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
