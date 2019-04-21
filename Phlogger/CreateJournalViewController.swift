65c9d4ba393c697cff66c5e9e490ec9fc72f1f07
Buffy Feinstein <buffyfeinstein@gmail.com>
2019-04-20 20:57:39 -0500

    keyboard toggle

--- Phlogger/CreateJournalViewController.swift
+++ Phlogger/CreateJournalViewController.swift
@@ -10,21 +10,61 @@ import UIKit
 
 class CreateJournalViewController: UIViewController {
 
+    @IBOutlet weak var aboveNavBarView: UIView!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var journalTextView: UITextView!
+    @IBOutlet weak var navBar: UINavigationBar!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var setDateButton: UIButton!
+    @IBOutlet weak var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        navBar.barTintColor = UIColor(red: 0.298, green: 0.757, blue: 0.988, alpha: 1.00) // #4cc1fc
+        navBar.tintColor = .white
+        navBar.isTranslucent = false
+        navBar.titleTextAttributes = [.foregroundColor : UIColor.white]
+        aboveNavBarView.backgroundColor = UIColor(red: 0.298, green: 0.757, blue: 0.988, alpha: 1.00) // #4cc1fc
+        
+        
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(KeyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(KeyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func KeyboardWillHide(notification: Notification){
+        changeKeyboardHeight(notification: notification)
+        
     }
-    */
+    
+    @objc func KeyboardWillShow(notification: Notification){
+        changeKeyboardHeight(notification: notification)
+        
+    }
+    
+    func changeKeyboardHeight(notification:Notification){
+        if let keyboardFrame =
+            notification.userInfo? [UIResponder.keyboardFrameEndUserInfoKey] as?
+            NSValue {
+            let keyHeight = keyboardFrame.cgRectValue.height
+            bottomConstraint.constant = keyHeight + 10
+            
+        }
+    }
+    
+    @IBAction func cancelTapped(_ sender: Any) {
+    }
+    @IBAction func saveTapped(_ sender: Any) {
+    }
+    @IBAction func setDateTapped(_ sender: Any) {
+    }
+    @IBAction func blackCalendarTapped(_ sender: Any) {
+    }
+    @IBAction func blackCameraTapped(_ sender: Any) {
+    }
+    
+    
 
 }
